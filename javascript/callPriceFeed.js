1732b0aa5ced48ff58786394b31619f88c40b324
nhaga <nhaga5@gmail.com>
2023-03-27 12:16:52 +0100

    updated KEY variable

--- javascript/callPriceFeed.js
+++ javascript/callPriceFeed.js
@@ -4,7 +4,7 @@ const abi = require('./Oracle.json').abi
 
 const rpcURL = "https://rpc-devnet-cardano-evm.c1.milkomeda.com"
 const provider = new ethers.JsonRpcProvider(rpcURL)
-const signer = new ethers.Wallet( `0x${process.env.PRIVATE_KEY0}`, provider )
+const signer = new ethers.Wallet( `0x${process.env.PRIVATE_KEY}`, provider )
 const oracle = new ethers.Contract("0x47a7d67e89E5714456b9af39703C1dc62203002A", abi, signer)
 
 
